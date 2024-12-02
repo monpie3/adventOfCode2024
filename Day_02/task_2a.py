4ef03c8dc381ee261908101b0a3e56a933bc5815
Monika <dotyk_zla@o2.pl>
2024-12-02 16:01:44 -0500

    Day 02 Part 1

---
+++ Day_02/task_2a.py
@@ -0,0 +1,32 @@
+def load_reports(filename):
+    reports = []
+    with open(filename, "r") as f:
+        for report in f.readlines():
+            report = report.strip().split(" ")
+            report = list(map(int, report))
+            reports.append(report)
+    return reports
+
+def check_difference(acc, value):
+    prev, is_valid = acc
+    if not is_valid:
+        return prev, False
+    return value, abs(prev - value) <= 3
+
+
+def is_save(report):
+    if sorted(report) != report and sorted(report, reverse=True) != report:
+        return False
+
+    if len(set(report)) != len(report):
+        return False
+
+    return all(abs(report[i] - report[i + 1]) <= 3 for i in range(len(report) - 1))
+
+
+if __name__ == "__main__":
+    reports = load_reports("puzzle_input.txt")
+    total = 0
+    for report in reports:
+        total += is_save(report)
+    print(total)
