f507d540788b9dc71ef72be17b489205fa42250e
Monika <dotyk_zla@o2.pl>
2024-12-07 19:56:03 -0500

    Day 06 Part 2, fix for part 1

---
+++ Day_06/task_6b.py
@@ -0,0 +1,129 @@
+import copy
+
+def load_lab_map(filename):
+    lab_map = ""
+    with open(filename, "r") as f:
+        lab_map = f.read()
+
+    return [list(row) for row in lab_map.split()]
+
+
+def up(row, col):
+    return row - 1, col
+
+
+def right(row, col):
+    return row, col + 1
+
+
+def left(row, col):
+    return row, col - 1
+
+
+def down(row, col):
+    return row + 1, col
+
+
+def find_guard_position(lab_map):
+    for index, row in enumerate(lab_map):
+        if "^" in row:
+            return index, row.index("^")
+        if "v" in row:
+            return index, row.index("v")
+        if ">" in row:
+            return index, row.index(">")
+        if "<" in row:
+            return index, row.index("<")
+
+
+def find_guard_direction(lab_map, position):
+    return lab_map[position[0]][position[1]]
+
+
+def is_leaving(cur_pos, direciton, rows, cols):
+    cur_x, cur_y = cur_pos
+    if direciton == "^" and cur_x == 0:
+        return True
+    if direciton == "v" and cur_x == rows - 1:
+        return True
+    if direciton == "<" and cur_y == 0:
+        return True
+    if direciton == ">" and cur_y == cols - 1:
+        return True
+    return False
+
+
+def patrol(lab_map, start_position, start_direction, ending_condition="leave"):
+    rows = len(lab_map)
+    cols = len(lab_map[0])
+
+    explored_positions = {start_position: [start_direction]}
+    left_patrol = False
+
+    x, y = start_position
+
+    while not left_patrol:
+        if lab_map[x][y] == "^":
+            next_x, next_y = up(x, y)
+            if lab_map[next_x][next_y] == "#":
+                lab_map[x][y] = ">"
+            else:
+                x, y = up(x, y)
+                lab_map[x][y] = "^"
+
+        elif lab_map[x][y] == "v":
+            next_x, next_y = down(x, y)
+            if lab_map[next_x][next_y] == "#":
+                lab_map[x][y] = "<"
+            else:
+                x, y = down(x, y)
+                lab_map[x][y] = "v"
+
+        elif lab_map[x][y] == "<":
+            next_x, next_y = left(x, y)
+            if lab_map[next_x][next_y] == "#":
+                lab_map[x][y] = "^"
+            else:
+                x, y = left(x, y)
+                lab_map[x][y] = "<"
+
+        elif lab_map[x][y] == ">":
+            next_x, next_y = right(x, y)
+            if lab_map[next_x][next_y] == "#":
+                lab_map[x][y] = "v"
+            else:
+                x, y = right(x, y)
+                lab_map[x][y] = ">"
+
+        left_patrol = is_leaving((x, y), lab_map[x][y], rows, cols)
+
+        if ending_condition == "loop":
+            if left_patrol == True:
+                return False
+
+            if lab_map[x][y] in explored_positions.get((x, y), []):
+                return True
+
+        if (x, y) not in explored_positions.keys():
+            explored_positions[(x, y)] = [lab_map[x][y]]
+    return explored_positions
+
+
+def add_obstructions(lab_map, current_area, start_position, start_direction):
+    total = 0
+    for obstacle in current_area:
+        modified_lab_map = copy.deepcopy(lab_map)
+        modified_lab_map[obstacle[0]][obstacle[1]] = "#"
+        total += patrol(modified_lab_map, start_position, start_direction, "loop")
+    return total
+
+
+if __name__ == "__main__":
+    lab_map = load_lab_map("Day_06/puzzle_input.txt")
+    patrol_map = copy.deepcopy(lab_map)
+    start_position = find_guard_position(lab_map)
+    start_direction = find_guard_direction(lab_map, start_position)
+
+    current_area = patrol(patrol_map, start_position, start_direction)
+    del current_area[start_position]
+    print(add_obstructions(lab_map, current_area, start_position, start_direction))
--- Day_06/test_task_6a.py
+++ Day_06/test_task_6a.py
@@ -4,3 +4,9 @@ from task_6a import load_lab_map, patrol
 def test_patrol():
     lab_map = load_lab_map("Day_06/example_6a.txt")
     assert len(patrol(lab_map)) == 41
+
+
+def test_patrol_when_guard_does_multiple_turns():
+    # https://www.reddit.com/r/adventofcode/comments/1h8vq51/2024_day_6_part_2_i_am_stumped/
+    lab_map = load_lab_map("Day_06/example_6b.txt")
+    assert len(patrol(lab_map)) == 2
